68a3e160aa2d94e43a22c7e55e4ee6172f77e0b5
@hexlet/cli <support@hexlet.io>
2023-02-10 21:52:44 +0300

    submit java-collections-ru/other-collections

---
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -0,0 +1,30 @@
+package exercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+// BEGIN
+class App {
+    public static Map<String, String> genDiff(Map<String, Object> firstDictionary, Map<String, Object> secondDictionary) {
+        Map<String, String> diff = new LinkedHashMap<>();
+
+        for (String key : firstDictionary.keySet()) {
+            if (secondDictionary.containsKey(key) && firstDictionary.get(key) == secondDictionary.get(key)) {
+                diff.put(key, "unchanged");
+            } else if (secondDictionary.containsKey(key)) {
+                diff.put(key, "changed");
+            } else {
+                diff.put(key, "deleted");
+            }
+        }
+
+        for (String key : secondDictionary.keySet()) {
+            if (!diff.containsKey(key)) {
+                diff.put(key, "added");
+            }
+        }
+
+        return diff;
+    }
+}
+//END
